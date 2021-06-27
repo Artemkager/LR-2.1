4dd8b52cf1a53e673c2747d0313267e2d271444c
airex <airex@DESKTOP-PAB22CS>
2021-06-27 20:18:17 +0300

    Добавьте файлы проекта.

---
+++ LR 2.1/LR 2.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+using namespace std;
+class LR {
+public:
+    string stroka(string c) {
+        string a = ""; // обозначение пустой строки
+        for (int i = 0; i < c.size(); i++) { //цикл для размерности строки
+            if (a.size() && a.back() == c[i]) { //back возвращается последний элемент, size вовращает длину строки
+                continue; // переделает управление строке
+            }
+            a += c[i]; // сжатие
+        }
+        return a;
+    }
+};
+int main() {
+    setlocale(LC_ALL, "RU");
+    string a;
+    LR b;
+    cout << "Введите слово/букву - ";
+    cin >> a;
+    cout << (b.stroka(a)) << " - Сжатая слово/буква" << endl;
+    cout << endl;
+    system("pause");
+    return 0;
